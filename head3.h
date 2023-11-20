66daa9486217c8443581cc642a5e2514791396d7
freecodetiger <145535186+freecodetiger@users.noreply.github.com>
2023-11-20 22:44:44 +0800

    Update head3.h将获取活细胞数目函数和演化函数放在一起

--- head3.h
+++ head3.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 using namespace std;
-//获取周围活细胞数目
+//获取周围活细胞数目并根据周围活细胞数目演化
 class getAliveNum{
 public:
 int getRoundAlive(int map[ROWS][COLS], int i, int j) {
@@ -15,5 +15,30 @@ int getRoundAlive(int map[ROWS][COLS], int i, int j) {
 	else if (i < ROWS - 1 && map[i + 1][j]) { num++; }//正下方
 	if (i < ROWS - 1 && j < COLS - 1 && map[i + 1][j + 1]) { num++; }//右下方
 	return num;
+}
+void lifeWeek(int map[ROWS][COLS]) {
+	//准备临时数组，用来存放一次演化后的细胞分布情况
+	int temp[ROWS][COLS] = { 0 };
+	int num = 0;
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			num = getRoundAlive(map, i, j);
+			if (3 == num) {//如果细胞周围有3个存活的细胞   
+				temp[i][j] = 1;//存活
+			}
+			else if (2 == num) {//如果细胞周围有2个存活的细胞   
+				temp[i][j] = map[i][j];//维持不变
+			}
+			else {//如果是其他情况			    
+				temp[i][j] = 0;//不存活
+			}
+		}
+	}
+	//将临时数组的细胞分布情况拷贝到原来数组上
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			map[i][j] = temp[i][j];
+		}
+	}
 }
 };
