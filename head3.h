2793569d5c4d920533fbc61817e9e038a25f362c
freecodetiger <145535186+freecodetiger@users.noreply.github.com>
2023-11-20 22:15:56 +0800

    Create head3.h用来获取一个格子周围活细胞的数目并返回
    这个头文件用来获取一个格子周围的活细胞数目

---
+++ head3.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+using namespace std;
+//获取周围活细胞数目
+int getRoundAlive(int map[ROWS][COLS], int i, int j) {
+	int num = 0;
+	if (i > 0 && j > 0 && map[i - 1][j - 1]) { num++; }//左上角
+	else if (i > 0 && map[i - 1][j]) { num++; }//正上方
+	else if (i > 0 && j < COLS - 1 && map[i - 1][j + 1]) { num++; }//右上方
+	else if (j > 0 && map[i][j - 1]) { num++; }//正左方
+	else if (j < COLS - 1 && map[i][j + 1]) { num++; }//正右方
+	else if (i < ROWS - 1 && j > 0 && map[i + 1][j - 1]) { num++; }//左下方
+	else if (i < ROWS - 1 && map[i + 1][j]) { num++; }//正下方
+	if (i < ROWS - 1 && j < COLS - 1 && map[i + 1][j + 1]) { num++; }//右下方
+	return num;
+}
