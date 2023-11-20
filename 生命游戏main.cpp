58b297f164e7ce80301e7b2b8dbf1fd714c105b8
freecodetiger <145535186+freecodetiger@users.noreply.github.com>
2023-11-20 22:52:48 +0800

    Update 生命游戏main.cpp

--- 生命游戏main.cpp
+++ 生命游戏main.cpp
@@ -1,27 +1,30 @@
 #include<iostream>
 #include<graphics.h>
 using namespace std;
-#include"head.h"
+#include"head1.h"
+#include"head2.h"
+#include"head3.h"
 //主函数
-//bool isContinue;
-//int map[ROWS][COLS];
+DrawMap a;
+GetAndSet b;
+getAliveNum c;
 int main(){
 	map[ROWS][COLS] = 0;
 	isContinue = true;
 	initgraph(COLS * SPACE, SPACE * ROWS);
 	while (isContinue) {
-		drawMap(map);
+		a.drawMap(map);
 		Sleep(10);
-		getAndSet(map);
+		b.getAndSet(map);
 	}
 	settextcolor(RED);
 	outtextxy(400, 500, "开始演示");
 	while (1) {//循环演化
 		//求出来每个细胞周围存活数量
 		//然后判断 设置
-		lifeWeek(map);
+		c.lifeWeek(map);
 		//显示
-		drawMap(map);
+		a.drawMap(map);
 		Sleep(10);//停顿百分之一秒
 	}
 return 0;
